7ecd4f2cd060f99dcdc97581d716ba70a64e4cae
BPEREIRA1995 <bruno.dhl@hotmail.com>
2026-02-22 13:49:15 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import BenefitsSection from "@/components/landing/BenefitsSection";
 import PricingSection from "@/components/landing/PricingSection";
 import FAQSection from "@/components/landing/FAQSection";
 import FooterCTA from "@/components/landing/FooterCTA";
+import VisitCounter from "@/components/landing/VisitCounter";
 
 const Index = () => {
   return (
@@ -16,6 +17,7 @@ const Index = () => {
       <PricingSection />
       <FAQSection />
       <FooterCTA />
+      <VisitCounter />
     </main>
   );
 };
